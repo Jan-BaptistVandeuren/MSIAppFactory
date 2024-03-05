916b4cfb04d67a7a8cabb8ae2add610f575c8025
Jan-BaptistVandeuren <jb.alexis@outlook.fr>
2024-03-05 14:46:18 +0100

    added some changes towads the build file and some changes towards the appfolderpath instead of jsonfilepath

--- Scripts/build.ps1
+++ Scripts/build.ps1
@@ -1,14 +1,11 @@
 param (
     [Parameter(Mandatory=$true)]
-    [string]$JsonFilePath
+    [string]$AppFolderPath
 )
 
-blob_name = ""
-file_type = ""
-container_name = ""
-
 # Parse the JSON file
-$jsonContent = Get-Content -Path $JsonFilePath | ConvertFrom-Json
+$jsonFilePath = Join-Path -Path $AppFolderPath -ChildPath "app.json"
+$jsonContent = Get-Content -Path $jsonFilePath | ConvertFrom-Json
 
 # Extract the relevant information from the JSON content
 $sourceFilesPath = $jsonContent.sourceFilesPath
@@ -16,13 +13,19 @@ $msiOutputPath = $jsonContent.msiOutputPath
 $msiParameters = $jsonContent.msiParameters
 $sourceStorageAccount = $jsonContent.sourceStorageAccount
 $destinationStorageAccount = $jsonContent.destinationStorageAccount
+$blobName = $jsonContent.blobName
+$fileType = $jsonContent.fileType
+$containerName = $jsonContent.containerName
+$sasToken = $jsonContent.sasToken
+$storageAccountBKey = $jsonContent.storageAccountBKey
 
 # Download files from the source Storage Account
-& 'az' 'storage' 'blob' 'download-batch' '--source' $sourceFilesPath '--destination' $msiOutputPath '--account-name' $sourceStorageAccount '--sas-token' $(sasToken)
+& 'az' 'storage' 'blob' 'download-batch' '--source' $sourceFilesPath '--destination' $msiOutputPath '--account-name' $sourceStorageAccount '--sas-token' $sasToken
 
 # Build the MSI
-& 'path\to\candle.exe' $sourceFilesPath -ext WixUIExtension
-& 'path\to\light.exe' $msiParameters -ext WixUIExtension
+$wxsFilePath = Join-Path -Path $AppFolderPath -ChildPath "app.wxs"
+& 'candle.exe' $wxsFilePath -ext WixUIExtension
+& 'light.exe' $msiParameters -ext WixUIExtension
 
 # Upload the MSI to the destination Storage Account
-& 'az' 'storage' 'blob' 'upload' '--account-name' $destinationStorageAccount '--account-key' $(storageAccountBKey) '--name' blob-name '--type' 'block' '--file-type' file-type '--file' $msiOutputPath '--container-name' container-name
+& 'az' 'storage' 'blob' 'upload' '--account-name' $destinationStorageAccount '--account-key' $storageAccountBKey '--name' $blobName '--type' 'block' '--file-type' $fileType '--file' $msiOutputPath '--container-name' $containerName
